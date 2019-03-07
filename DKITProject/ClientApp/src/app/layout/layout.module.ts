6e0f73343b996e7ed7007f9f13fe2eaba93421e4
reaper <Nikita.antipkin2014@yandex.ru>
2019-03-08 01:20:25 +0300

    refactoring

--- DKITProject/ClientApp/src/app/layout/layout.module.ts
+++ DKITProject/ClientApp/src/app/layout/layout.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from "@angular/core";
 import { RouterModule } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatInputModule } from '@angular/material';
 
 import { LoginFormComponent } from './components/login-form/login-form.component';
 import { MenuComponent } from './components/menu/menu.component';
 import { HeaderComponent } from './../layout/components/header/header.component';
+import { from } from 'rxjs';
 
 @NgModule({
     declarations: [
@@ -16,7 +19,9 @@ import { HeaderComponent } from './../layout/components/header/header.component'
     imports: [
         RouterModule,
         BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
-        ReactiveFormsModule
+        ReactiveFormsModule,
+        BrowserAnimationsModule,
+        MatInputModule
     ],
     exports: [
         HeaderComponent,
